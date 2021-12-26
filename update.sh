2d3896d11f1be6ef81779820a6a2876393f04e9e
MarioWi <mario.wicke@online.de>
2021-12-26 12:14:40 +0100

    Update update.sh
    make some notes in update script

---
+++ update.sh
@@ -0,0 +1,15 @@
+# rename local versions.csv --> versions.bak (maybee deleate existing *.bak)
+#
+# curl actual versions.csv from github (install_path)
+#
+# check every line in new versions.csv if already in *.bak
+# if not save with marker new in temp file or var
+# if newer save with old version --> new version
+# 
+# check-dialog with new versions and newer versions 
+# 
+# copy (override) checked *.jar.conf
+#  
+# 
+# 
+# 
